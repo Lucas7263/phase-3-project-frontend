7891707ab5b834a1d00c9ffcef78a1aee01e74e6
Lucas <lucaswoodyard@yahoo.com>
2023-05-29 15:34:29 -0500

    Deleted some files/folders and added a Components file to hold all future components. Also tested out retrieving data from my server to ensure I could grab everything.

---
+++ src/Components/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+
+
+function App() {
+  const [spaData, setSpaData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/spas`)
+    .then(res => res.json())
+    .then(data => setSpaData(data))
+  }, []);
+
+  const spas = spaData.map(spa => {
+    console.log(spa.dogs)
+      return (
+        <div>
+           <h3 key={spa.id}>{spa.name}</h3>
+           <h4 key={spa.id}>{spa.location}</h4>
+           <h5 key={spa.id}>{spa.price}</h5>
+           
+        <img key={spa.id} src={spa.image} alt={spa.name}></img>
+          
+            {spa.dogs.map(dog => (
+             <div key={spa.id}>
+              <ul>
+                <li>
+                {dog.dog_name}
+                </li>
+                <li> {dog.breed}</li>
+                </ul>
+              
+           </div>  
+          
+  ))}
+          
+        </div>
+       
+      )
+            
+  })
+   
+ console.log(spaData)
+
+  return (
+    <div>
+      <h2>Phase-3 Project</h2>
+      <p>{spas}</p>
+    </div>
+  );
+}
+
+export default App;
